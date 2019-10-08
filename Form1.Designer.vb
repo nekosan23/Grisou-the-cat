9970a78fcf396dd6eaaa12a34a3ee22bb2707805
Guillaume <quintinguillaume@outlook.com>
2019-10-08 18:04:40 -0400

    First Github upload
    There still a lot missing but each update will be added as it get done

---
+++ Form1.Designer.vb
@@ -0,0 +1,209 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class MainWin
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.MainDisplay = New System.Windows.Forms.Panel()
+        Me.AnnouncerPanel = New System.Windows.Forms.Panel()
+        Me.AnnouncerText = New System.Windows.Forms.Label()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.GrisouHealthBar = New System.Windows.Forms.ProgressBar()
+        Me.GrisouText2 = New System.Windows.Forms.Label()
+        Me.GrisouText1 = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.PlayerHealthBar = New System.Windows.Forms.ProgressBar()
+        Me.PlayerText2 = New System.Windows.Forms.Label()
+        Me.PlayerText1 = New System.Windows.Forms.Label()
+        Me.PlayerAction3 = New System.Windows.Forms.Button()
+        Me.PlayerAction2 = New System.Windows.Forms.Button()
+        Me.PlayerAction1 = New System.Windows.Forms.Button()
+        Me.MainDisplay.SuspendLayout()
+        Me.AnnouncerPanel.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MainDisplay
+        '
+        Me.MainDisplay.BackgroundImage = Global.WindowsApp2.My.Resources.Resources.grisouporn
+        Me.MainDisplay.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.MainDisplay.Controls.Add(Me.AnnouncerPanel)
+        Me.MainDisplay.Controls.Add(Me.Panel2)
+        Me.MainDisplay.Controls.Add(Me.Panel1)
+        Me.MainDisplay.Controls.Add(Me.PlayerAction3)
+        Me.MainDisplay.Controls.Add(Me.PlayerAction2)
+        Me.MainDisplay.Controls.Add(Me.PlayerAction1)
+        Me.MainDisplay.Location = New System.Drawing.Point(0, 0)
+        Me.MainDisplay.Name = "MainDisplay"
+        Me.MainDisplay.Size = New System.Drawing.Size(480, 720)
+        Me.MainDisplay.TabIndex = 0
+        '
+        'AnnouncerPanel
+        '
+        Me.AnnouncerPanel.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.AnnouncerPanel.Controls.Add(Me.AnnouncerText)
+        Me.AnnouncerPanel.Location = New System.Drawing.Point(0, 292)
+        Me.AnnouncerPanel.Name = "AnnouncerPanel"
+        Me.AnnouncerPanel.Size = New System.Drawing.Size(480, 51)
+        Me.AnnouncerPanel.TabIndex = 6
+        '
+        'AnnouncerText
+        '
+        Me.AnnouncerText.AutoSize = True
+        Me.AnnouncerText.Location = New System.Drawing.Point(12, 17)
+        Me.AnnouncerText.Name = "AnnouncerText"
+        Me.AnnouncerText.Size = New System.Drawing.Size(51, 17)
+        Me.AnnouncerText.TabIndex = 0
+        Me.AnnouncerText.Text = "Label1"
+        '
+        'Panel2
+        '
+        Me.Panel2.Controls.Add(Me.GrisouHealthBar)
+        Me.Panel2.Controls.Add(Me.GrisouText2)
+        Me.Panel2.Controls.Add(Me.GrisouText1)
+        Me.Panel2.Location = New System.Drawing.Point(268, 12)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(200, 77)
+        Me.Panel2.TabIndex = 4
+        '
+        'GrisouHealthBar
+        '
+        Me.GrisouHealthBar.Location = New System.Drawing.Point(0, 27)
+        Me.GrisouHealthBar.Maximum = 50
+        Me.GrisouHealthBar.Name = "GrisouHealthBar"
+        Me.GrisouHealthBar.Size = New System.Drawing.Size(200, 23)
+        Me.GrisouHealthBar.TabIndex = 2
+        '
+        'GrisouText2
+        '
+        Me.GrisouText2.AutoSize = True
+        Me.GrisouText2.Location = New System.Drawing.Point(146, 56)
+        Me.GrisouText2.Name = "GrisouText2"
+        Me.GrisouText2.Size = New System.Drawing.Size(0, 17)
+        Me.GrisouText2.TabIndex = 1
+        '
+        'GrisouText1
+        '
+        Me.GrisouText1.AutoSize = True
+        Me.GrisouText1.Location = New System.Drawing.Point(4, 3)
+        Me.GrisouText1.Name = "GrisouText1"
+        Me.GrisouText1.Size = New System.Drawing.Size(0, 17)
+        Me.GrisouText1.TabIndex = 0
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.PlayerHealthBar)
+        Me.Panel1.Controls.Add(Me.PlayerText2)
+        Me.Panel1.Controls.Add(Me.PlayerText1)
+        Me.Panel1.Location = New System.Drawing.Point(12, 540)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(200, 75)
+        Me.Panel1.TabIndex = 3
+        '
+        'PlayerHealthBar
+        '
+        Me.PlayerHealthBar.Location = New System.Drawing.Point(0, 23)
+        Me.PlayerHealthBar.Maximum = 50
+        Me.PlayerHealthBar.Name = "PlayerHealthBar"
+        Me.PlayerHealthBar.Size = New System.Drawing.Size(200, 23)
+        Me.PlayerHealthBar.TabIndex = 3
+        '
+        'PlayerText2
+        '
+        Me.PlayerText2.AutoSize = True
+        Me.PlayerText2.Location = New System.Drawing.Point(146, 50)
+        Me.PlayerText2.Name = "PlayerText2"
+        Me.PlayerText2.Size = New System.Drawing.Size(0, 17)
+        Me.PlayerText2.TabIndex = 2
+        '
+        'PlayerText1
+        '
+        Me.PlayerText1.AutoSize = True
+        Me.PlayerText1.Location = New System.Drawing.Point(3, 3)
+        Me.PlayerText1.Name = "PlayerText1"
+        Me.PlayerText1.Size = New System.Drawing.Size(0, 17)
+        Me.PlayerText1.TabIndex = 2
+        '
+        'PlayerAction3
+        '
+        Me.PlayerAction3.Enabled = False
+        Me.PlayerAction3.Location = New System.Drawing.Point(326, 631)
+        Me.PlayerAction3.Name = "PlayerAction3"
+        Me.PlayerAction3.Size = New System.Drawing.Size(142, 77)
+        Me.PlayerAction3.TabIndex = 2
+        Me.PlayerAction3.Text = "Recover"
+        Me.PlayerAction3.UseVisualStyleBackColor = True
+        '
+        'PlayerAction2
+        '
+        Me.PlayerAction2.Enabled = False
+        Me.PlayerAction2.Location = New System.Drawing.Point(172, 631)
+        Me.PlayerAction2.Name = "PlayerAction2"
+        Me.PlayerAction2.Size = New System.Drawing.Size(148, 77)
+        Me.PlayerAction2.TabIndex = 1
+        Me.PlayerAction2.Text = "Defense"
+        Me.PlayerAction2.UseVisualStyleBackColor = True
+        '
+        'PlayerAction1
+        '
+        Me.PlayerAction1.Location = New System.Drawing.Point(12, 631)
+        Me.PlayerAction1.Name = "PlayerAction1"
+        Me.PlayerAction1.Size = New System.Drawing.Size(154, 77)
+        Me.PlayerAction1.TabIndex = 0
+        Me.PlayerAction1.Text = "Attaque"
+        Me.PlayerAction1.UseVisualStyleBackColor = True
+        '
+        'MainWin
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(480, 720)
+        Me.Controls.Add(Me.MainDisplay)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "MainWin"
+        Me.Text = "Jeux de merde a sam"
+        Me.MainDisplay.ResumeLayout(False)
+        Me.AnnouncerPanel.ResumeLayout(False)
+        Me.AnnouncerPanel.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents MainDisplay As Panel
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents PlayerAction3 As Button
+    Friend WithEvents PlayerAction2 As Button
+    Friend WithEvents PlayerAction1 As Button
+    Friend WithEvents GrisouHealthBar As ProgressBar
+    Friend WithEvents GrisouText2 As Label
+    Friend WithEvents GrisouText1 As Label
+    Friend WithEvents PlayerHealthBar As ProgressBar
+    Friend WithEvents PlayerText2 As Label
+    Friend WithEvents PlayerText1 As Label
+    Friend WithEvents AnnouncerPanel As Panel
+    Friend WithEvents AnnouncerText As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,171 @@
+﻿Imports System.Threading.Thread
+Public Class MainWin
+    Public GrisouHealth, PlayerHealth, GrisouRoundDefense, PlayerRoundDefense As Integer
+    Public GameState As Boolean
+    Public GrisouTextA, PlayerTextA As String
+    Public Sub Startup(sender As Object, e As EventArgs) Handles MyBase.Load
+        GrisouHealth = 50
+        PlayerHealth = 50
+        'Setting up Grisou 
+        GrisouHealthBar.Value = GrisouHealth
+        GrisouText1.Text = "Grisou"
+        GrisouText2.Text = GrisouHealth.ToString + " / 50"
+        GrisouTextA = "Grisou Turn !"
+        'Setting up Player
+        PlayerHealthBar.Value = PlayerHealth
+        PlayerText1.Text = "Sam"
+        PlayerText2.Text = PlayerHealth.ToString + " / 50"
+        PlayerTextA = "Player turn !"
+        AnnouncerPanel.Visible = False
+        AnnouncerText.Visible = False
+        GameState = True
+        'Round()
+    End Sub
+    'Round system need to be active at any time or else game break
+    Public Sub Round()
+        Dim TurnValue As Integer
+        If (GameState = True) Then
+            'Game Just Got started
+
+            'Rolling dice who's starting
+            TurnValue = GetRandom(1, 2)
+        ElseIf (GameState = False) Then
+            'Game Already Started
+
+            'if this get called it mean the person did his turn so let change the value
+            If (TurnValue = 1) Then
+                TurnValue = 2
+            ElseIf (TurnValue = 2) Then
+                TurnValue = 1
+            End If
+        Else
+                MsgBox("GameState is not found")
+        End If
+        'Checking who's turn it is
+        If (TurnValue = 1) Then
+            'player is starting
+            PlayerAction1.Enabled = True
+            PlayerAction2.Enabled = True
+            PlayerAction3.Enabled = True
+            Announcer(PlayerTextA)
+        ElseIf (TurnValue = 2) Then
+            'Grisou is starting
+            PlayerAction1.Enabled = False
+            PlayerAction2.Enabled = False
+            PlayerAction3.Enabled = False
+            Announcer(GrisouTextA)
+        End If
+    End Sub
+    'Grisou AI yes we made a cat intelligent
+    Public Sub GrisouAI()
+
+    End Sub
+
+    'attack system cast at every attack never forget to mention the user casting
+    Public Sub Attack(ByVal user As Integer)
+        'setting up variable
+        Static Dim AttackLevel As Integer
+        Static Dim AttackLow = New Integer() {1, 2, 3, 4}
+        Static Dim AttackMedium = New Integer() {7, 9, 11, 13}
+        Static Dim AttackHigh = New Integer() {16, 17, 19, 20}
+        Static Dim Container1 As Integer
+        AttackLevel = GetRandom(1, 4)
+        Select Case AttackLevel
+            Case 1
+                'Hit or miss i guess they never miss humm
+                If (user = 1) Then
+                    Announcer("Your attack missed")
+                Else
+                    Announcer("Grisou attack missed")
+                End If
+            Case 2
+                'Low
+                Container1 = GetRandom(1, 4)
+                If (user = 1) Then
+                    Announcer("Your attack did " + AttackLow(Container1) + " damage")
+                    DataProcess(2, "Attack", AttackLow(Container1))
+                Else
+                    Announcer("Grisou attack did " + AttackLow(Container1) + " damage")
+                    DataProcess(1, "Attack", AttackLow(Container1))
+                End If
+            Case 3
+                'Medium
+                Container1 = GetRandom(1, 4)
+                If (user = 1) Then
+                    Announcer("Your attack did " + AttackMedium(Container1) + " damage")
+                    DataProcess(2, "Attack", AttackMedium(Container1))
+                Else
+                    Announcer("Grisou attack did " + AttackMedium(Container1) + " damage")
+                    DataProcess(1, "Attack", AttackMedium(Container1))
+                End If
+            Case 4
+                'High
+                Container1 = GetRandom(1, 4)
+                If (user = 1) Then
+                    Announcer("Your attack did " + AttackHigh(Container1) + " damage")
+                    DataProcess(2, "Attack", AttackHigh(Container1))
+                Else
+                    Announcer("Grisou attack did " + AttackHigh(Container1) + " damage")
+                    DataProcess(1, "Attack", AttackHigh(Container1))
+                End If
+        End Select
+    End Sub
+    'Defense System cast every defense never forget to mention the user casting
+    Public Sub Defense(ByVal user As Integer)
+        Static Dim Defense = New Integer() {6, 8, 12, 14}
+        Static Dim Container1 As Integer
+        Container1 = GetRandom(1, 4)
+        If (user = 1) Then
+            Announcer("You casted defense and got " + Defense(Container1) + " defense for this turn")
+            DataProcess(1, "Defense", Defense(Container1))
+        Else
+            Announcer("Grisou casted defense and got " + Defense(Container1) + " defense for this turn")
+            DataProcess(2, "Defense", Defense(Container1))
+        End If
+    End Sub
+    'Data Processer
+    'target list    1 player    2 grisou
+    'DataType   Attack  Defense  Recovery
+    Public Sub DataProcess(ByVal Target As Integer, ByVal DataType As String, ByVal DataValue As Integer)
+
+    End Sub
+    'Announcer
+    Public Sub Announcer(text As String) '294
+        Dim animationcountdown As Integer = -490
+        AnnouncerText.Text = text
+        AnnouncerText.Visible = True
+        AnnouncerPanel.Visible = True
+        Do
+            AnnouncerPanel.Location = New Point(animationcountdown, 292)
+            animationcountdown += 10
+            Sleep(10)
+        Loop Until animationcountdown = 10
+        AnnouncerText.Refresh()
+        Sleep(3000)
+        AnnouncerText.Visible = False
+        AnnouncerPanel.Visible = False
+    End Sub
+    'Random Generator for integer
+    Public Function GetRandom(ByVal Min As Integer, ByVal Max As Integer) As Integer
+        Static Generator As System.Random = New System.Random
+        Return Generator.Next(Min, Max)
+    End Function
+    Private Sub PlayerAttackClick(sender As Object, e As EventArgs) Handles PlayerAction1.Click
+        'PlayerAction1.Enabled = False
+        'PlayerAction2.Enabled = False
+        'PlayerAction3.Enabled = False
+        'Attack(1)
+    End Sub
+    Private Sub PlayerDefenseClick(sender As Object, e As EventArgs) Handles PlayerAction2.Click
+        PlayerAction1.Enabled = False
+        PlayerAction2.Enabled = False
+        PlayerAction3.Enabled = False
+        'defense(1)
+    End Sub
+    Private Sub PlayerRecoverClick(sender As Object, e As EventArgs) Handles PlayerAction3.Click
+        PlayerAction1.Enabled = False
+        PlayerAction2.Enabled = False
+        PlayerAction3.Enabled = False
+        'recover(1)
+    End Sub
+End Class
